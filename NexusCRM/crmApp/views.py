56ef4b40725c8d282319bbeee5f728060b714e49
Cptzeph23 <asekazephaniah@gmail.com>
2026-08-16 16:47:00 +0300

    dashboard

--- NexusCRM/crmApp/views.py
+++ NexusCRM/crmApp/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'dashboard.html')
 
+def module_placeholder(request, module_name):
+    return render(
+        request,
+        "placeholder.html",
+        {
+            "module_name": module_name,
+        }
+    )
