9f0aaffe80542847f3e8a78b5aa2ed175f170e20
Cptzeph23 <asekazephaniah@gmail.com>
2026-08-21 15:20:15 +0300

    pipeline amount validator

--- NexusCRM/crmApp/models.py
+++ NexusCRM/crmApp/models.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
-
+from django.core.validators import MinValueValidator
 
 class Profile(models.Model):
 
@@ -454,7 +454,10 @@ class Deal(models.Model):
     amount = models.DecimalField(
         max_digits=15,
         decimal_places=2,
-        default=Decimal("0.00")
+        default=Decimal("0.00"),
+        validators=[
+            MinValueValidator(Decimal("0.00"))
+        ]
     )
 
     expected_close_date = models.DateField(
