cebd2f583c68080eddf841a460ff619cddafd786
Cptzeph23 <asekazephaniah@gmail.com>
2026-08-24 12:15:15 +0300

    task urls

--- NexusCRM/crmApp/urls.py
+++ NexusCRM/crmApp/urls.py
@@ -64,8 +64,12 @@ urlpatterns = [
     path("activities/<int:pk>/delete/",views.activity_delete,name="activity_delete"),
 
     
-    # 
-    path("tasks/", lambda request: views.module_placeholder( request,"Tasks"), name="tasks"),
+    # Tasks
+    # path("tasks/", views.task_list, name="tasks"),
+    # path("tasks/create/", views.task_create, name="task_create"),
+    # path("tasks/<int:pk>/", views.task_detail, name="task_detail"),
+    # path("tasks/<int:pk>/edit/", views.task_edit, name="task_edit"),
+    # path("tasks/<int:pk>/delete/", views.task_delete, name="task_delete"),
 
     path("reports/", lambda request: views.module_placeholder(request,"Reports"), name="reports"),
 
