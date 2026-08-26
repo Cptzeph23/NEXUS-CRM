517492d3af0947b601aa7ca1b8f8f94edfe8de7d
Cptzeph23 <asekazephaniah@gmail.com>
2026-08-26 09:30:49 +0300

    notification icon

--- NexusCRM/NexusCRM/settings.py
+++ NexusCRM/NexusCRM/settings.py
@@ -52,6 +52,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 "crmApp.context_processors.crm_permissions",
+                "crmApp.context_processors.notification_context",
             ],
         },
     },
