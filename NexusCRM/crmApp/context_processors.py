871e207aa343067a2038544fee96cbf30bd43283
Cptzeph23 <asekazephaniah@gmail.com>
2026-08-26 09:05:43 +0300

    context processors

--- NexusCRM/crmApp/context_processors.py
+++ NexusCRM/crmApp/context_processors.py
@@ -6,6 +6,25 @@ from .permissions import (
     ROLE_SUPPORT,
 )
 
+from .models import Notification
+
+
+def notification_context(request):
+
+    if not request.user.is_authenticated:
+        return {
+            "unread_notification_count": 0,
+        }
+
+    return {
+        "unread_notification_count": (
+            Notification.objects.filter(
+                recipient=request.user,
+                is_read=False
+            ).count()
+        ),
+    }
+
 
 def crm_permissions(request):
     """
